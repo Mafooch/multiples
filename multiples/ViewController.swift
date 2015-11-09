500f58dbca22ecfbc919509b3e945a3eeeb96cd5
Joshua Maffuccio <joshua.maffuccio@raizlabs.com>
2015-11-09 12:01:35 -0500

    add and connect outlets

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    // Outlets
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var whatMultiple: UITextField!
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var multiplesLbl: UILabel!
 }
 
